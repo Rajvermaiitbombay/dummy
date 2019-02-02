7c022e0c240b40bc7d7c4cd635ff57eb23ee175d
rajkumar <raj345051.com>
2019-02-02 18:21:28 +0530

    ML

--- FlaskApp/data/ML_project.py
+++ FlaskApp/data/ML_project.py
@@ -1,28 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 26 16:21:06 2018
-
 @author: Rajkumar
 """
 import pandas as pd
-import glob
 import numpy as np
-import sklearn
+#import sklearn
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV, train_test_split, cross_validate
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 
 # Read CSV file #####
-lsp = pd.read_csv('D:\Rajkumar\Python\FlaskApp\data\lsp.csv')
+lsp = pd.read_csv('D:\Raj_kumar\Rajkumar\learninGit\dummy\FlaskApp\data\lsp.csv')
+df = pd.read_excel('D:\Raj_kumar\Rajkumar\learninGit\dummy\FlaskApp\data\winequality-white.xlsx')
 
 ## Split the training and tesing datasets from main datasets
-test= lsp.iloc[11:16,1:10] 
-train = lsp.iloc[np.r_[1:11,16:21],3:10]
+#test= lsp.iloc[11:16,1:10] 
+#train = lsp.iloc[np.r_[1:11,16:21],3:10]
+X,y = df.iloc[:,1:11], df.iloc[:,11]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
 
 ## import support vector machine #####
 from sklearn.svm import SVC
-svc= SVC(kernel='rbf')
-svc.fit(train.iloc[:,0:6],train.iloc[:,6])
-pred = svc.predict(test.iloc[:,2:8])
+parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+svc= SVC()
+## gridsearchCV & cross_validate
+clf = GridSearchCV(svc, parameters, cv=3)
+#svc.fit(train.iloc[:,0:6],train.iloc[:,6])
+clf.fit(X_train, y_train)
+#pred = svc.predict(test.iloc[:,2:8])
+pred = clf.predict(X_test)
+
+
+## confusion matrix
+confusion_matrix(y_test, pred)
+tn, fp, fn, tp = confusion_matrix(y_test, pred).ravel()
+
+## precision & recall of true positive
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+
+## precision & recall of true negative
+precision = tn / (tn + fn)
+recall = tn / (tn + fp)
+
+## accuracy of model
+accuracy_score(y_test, pred)*100
+
 
 #test1 = pd.DataFrame(test,index=[0,1,2,3,4])
 pred1 = pd.DataFrame(pred)
